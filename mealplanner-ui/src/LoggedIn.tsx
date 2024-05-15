4faf18e8b786afb2a812aedb92b8e9a9406cc6c8
Rodrigo Castro <castro.rl@gmail.com>
2024-05-15 15:26:14 -0300

    Updating the logic to redirect the logged in user to the terms and conditions page, or back to the /

--- mealplanner-ui/src/LoggedIn.tsx
+++ mealplanner-ui/src/LoggedIn.tsx
@@ -1,21 +1,18 @@
 // Ensure the user is logged in before routing to meal plans and such authorised pages.
 import * as React from "react";
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 import { getCurrentPerson } from "./state/state";
-import { useLocation } from "react-router-dom";
+import { useState } from 'react';
 
 export const LoggedIn = ({ children }: { children: React.ReactNode }) => {
   console.log(`current Person ID in LoggedIn ${getCurrentPerson()}`);
-  const location = useLocation();
-  const isLogoutPage = location.pathname === '/';
-  const isTermsPage = location.pathname === '/terms';
-  const isMealsPage = location.pathname === '/meals';
+  const [currentPerson, setCurrentPerson] = useState(getCurrentPerson());
 
-  if(isMealsPage && getCurrentPerson().personID === "")
-    return <Navigate to="/meals" replace/>;
-  if (getCurrentPerson().personID === "") return <Navigate to="/" />;
-    if (!isLogoutPage && !isTermsPage && !getCurrentPerson().personTerms) {
-      return <Navigate to = "/terms" replace/>;
-    }
+  if (currentPerson.personID === "" ) {
+    return <Navigate to="/" />;
+  } else if (currentPerson.personTerms === false) {
+    return <Navigate to="/terms" replace/>;
+  } 
+
   return <React.Fragment>{children}</React.Fragment>;
 };
